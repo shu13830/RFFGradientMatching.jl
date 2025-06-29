172330f50f6d97492df1cdb25352091fb4b694c0
Documenter.jl <documenter@juliadocs.github.io>
2025-06-29 12:13:17 +0000

    build based on a10a102

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RFFGradientMatching","page":"Home","title":"RFFGradientMatching","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RFFGradientMatching.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
